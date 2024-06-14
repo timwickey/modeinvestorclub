65e7806c7009792f554a974d3d09c92ebccbe3a6
Tim Wickey <tim@wickeyappstore.com>
2024-06-14 11:30:48 -0400

    UPDATE: sign in screen

--- lib/src/screens/sign_in.dart
+++ lib/src/screens/sign_in.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import '../backend.dart';
 
 class Credentials {
   final String email;
@@ -25,6 +24,43 @@ class SignInScreen extends StatefulWidget {
 class _SignInScreenState extends State<SignInScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+
+  Future<void> _signIn() async {
+    final email = _emailController.value.text;
+    final password = _passwordController.value.text;
+
+    String url = 'https://nodejs-serverless-connector.vercel.app/api/login';
+    Map<String, String> body = {'email': email, 'password': password};
+    ApiResult<ApiResponse> result =
+        await asyncCallApiData(url, method: 'POST', body: body);
+
+    if (!mounted) return;
+
+    if (result.data != null) {
+      // Handle successful response
+      ApiResponse data = result.data!;
+      print('Login successful. Token: ${data.token}');
+      print('User ID: ${data.id}');
+      print('First Name: ${data.firstName}');
+      print('Last Name: ${data.lastName}');
+      print('Email: ${data.email}');
+
+      // Call the onSignIn callback with the credentials
+      widget.onSignIn(Credentials(email, password));
+      // Handle success (e.g., navigate to another screen, show success message, etc.)
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Login successful')),
+      );
+    } else {
+      // Handle error
+      String error = result.error!;
+      print('Error: $error');
+      // Handle error (e.g., show error message)
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Login failed: $error')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -61,41 +97,86 @@ class _SignInScreenState extends State<SignInScreen> {
           ),
         ),
       );
+}
 
-  Future<void> _signIn() async {
-    // _apiResponse = asyncCallApiData(
-    //     'https://nodejs-serverless-connector.vercel.app/api/hello');
-    final email = _emailController.value.text;
-    final password = _passwordController.value.text;
+class ApiResponse {
+  final int id;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String message;
+  final String token;
 
-    String url = 'https://nodejs-serverless-connector.vercel.app/api/login';
-    Map<String, String> body = {'email': email, 'password': password};
-    ApiResult<ApiResponse> result =
-        await asyncCallApiData(url, method: 'POST', body: body);
+  ApiResponse({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.message,
+    required this.token,
+  });
 
-    if (result.data != null) {
-      // Handle successful response
-      ApiResponse data = result.data!;
-      print('Login successful. Token: ${data.token}');
-      print('User ID: ${data.id}');
-      print('First Name: ${data.firstName}');
-      print('Last Name: ${data.lastName}');
-      print('Email: ${data.email}');
+  factory ApiResponse.fromJson(Map<String, dynamic> json) {
+    return ApiResponse(
+      id: json['user']['id'],
+      firstName: json['user']['firstname'],
+      lastName: json['user']['lastname'],
+      email: json['user']['email'],
+      message: json['message'],
+      token: json['token'],
+    );
+  }
+}
 
-      // Call the onSignIn callback with the credentials
-      widget.onSignIn(Credentials(email, password));
-      // Handle success (e.g., navigate to another screen, show success message, etc.)
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Login successful')),
-      );
+class ApiResult<T> {
+  final T? data;
+  final String? error;
+
+  ApiResult({this.data, this.error});
+}
+
+Future<ApiResult<ApiResponse>> asyncCallApiData(String endpointUrl,
+    {String method = 'GET', Map<String, String>? body}) async {
+  // Ensure endpointUrl is a valid String
+  if (endpointUrl.isEmpty) {
+    return ApiResult(error: 'Endpoint URL must be a non-empty string.');
+  }
+
+  // Ensure method is either 'GET' or 'POST'
+  if (method != 'GET' && method != 'POST') {
+    return ApiResult(error: 'Method must be either "GET" or "POST".');
+  }
+
+  Uri uri = Uri.parse(endpointUrl);
+  final client = http.Client();
+  http.Response response;
+
+  try {
+    if (method == 'GET') {
+      response = await client.get(uri);
     } else {
-      // Handle error
-      String error = result.error!;
-      print('Error: $error');
-      // Handle error (e.g., show error message)
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Login failed')),
+      response = await client.post(
+        uri,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(body),
       );
     }
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> jsonResponse = json.decode(response.body);
+
+      ApiResponse data = ApiResponse.fromJson(jsonResponse);
+
+      return ApiResult(data: data); // Return the parsed data
+    } else {
+      String errorBody = response.body;
+      return ApiResult(
+          error:
+              'Failed to load data: ${response.reasonPhrase}. Error body: $errorBody');
+    }
+  } catch (e) {
+    return ApiResult(error: 'Failed to load data: $e');
+  } finally {
+    client.close();
   }
 }
