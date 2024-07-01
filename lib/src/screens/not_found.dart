932d1b0df7a14a879d55f2ec53079e67df7f61b8
Tim Wickey <tim@wickeyappstore.com>
2024-07-01 15:08:04 -0400

    UPDATE: new 404 page

--- lib/src/screens/not_found.dart
+++ lib/src/screens/not_found.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../widgets/ui.dart';
+import 'package:go_router/go_router.dart';
 
 class NotFoundScreen extends StatelessWidget {
   const NotFoundScreen({super.key});
@@ -6,11 +8,65 @@ class NotFoundScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Page Not Found')),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text('Page Not Found'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
       body: Center(
-        child: Text(
-          '404 - Page Not Found',
-          style: Theme.of(context).textTheme.headlineLarge,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.error_outline,
+                color: Colors.redAccent,
+                size: 100,
+              ),
+              const SizedBox(height: 24),
+              const Text(
+                '404',
+                style: TextStyle(
+                  fontSize: 72,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.redAccent,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 10.0,
+                      color: Colors.red,
+                      offset: Offset(5.0, 5.0),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                'Page Not Found',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                'The page you are looking for does not exist.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[400],
+                ),
+              ),
+              const SizedBox(height: 32),
+              SquaredButton(
+                  icon: Icon(Icons.home),
+                  text: "Go to Home",
+                  onPressed: () {
+                    GoRouter.of(context).go('/home');
+                  }),
+            ],
+          ),
         ),
       ),
     );
